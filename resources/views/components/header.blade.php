b05e36272208a34dd8969f9e58453d3c8b72cc81
Alexandra Justine Tuliao <avtuliao@up.edu.ph>
2021-01-13 22:58:10 +0800

    Removed logout button if user is not authenticated

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -2,7 +2,9 @@
     <a href="/" class="w-5 md:w-1/12"><img src="{{ asset('img/intechlogo.svg') }}" alt="Logo"></a>
     <h2 class="font-sans font-bold text-xl md:text-6xl tracking-wide text-center text-white ml-8">UP INTECH</h2>
     <h2 class="font-sans font-extralight text-xl md:text-4xl w-6/12 tracking-wide text-center text-white -ml-12 mt-1">Membership Monitoring System</h2>
+    @if (Auth::user())
     <div class="flex flex-row justify-end">
         <a href="/logout" class="bg-blue-400 hover:bg-indigo-100 border border-blue-500 md:text-lg text-center rounded-sm font-semibold text-white w-20 h-8 -mt-10 -mr-5"><img src="">Logout</a>
     </div>
+    @endif
 </div>
