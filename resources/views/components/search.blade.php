472d0b95b3d7c528fbfbe21370b76fa90889761e
Alexandra Justine <59907792+ajvtuliao@users.noreply.github.com>
2021-01-15 05:05:36 +0800

    Update search.blade.php

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -2,7 +2,7 @@
     <form action="/search" method="post" class="flex flex-row w-full space-x-4">
         @csrf
         <input type="search" name="search" placeholder="Search Name" class="border w-full text-gray-500 px-2 font-light border-indigo-300 bg-indigo-50 rounded focus:ring-2 h-8">
-        <input type="submit" class="bg-blue-700 hover:bg-blue-900 border border-indigo-200 text-blue-50 font-semibold rounded h-8 w-40 -pt-3" value="Search">
+        <input type="submit" class="bg-blue-700 hover:bg-blue-900 border border-indigo-200 text-blue-50 font-semibold rounded h-8 w-40" value="Search">
         <div class="flex flex-row justify-center">
             @if (Auth::user()->is_admin==1)
             <a href="#create" rel="modal:open" class=" text-green-50 bg-green-700 hover:bg-green-900 font-semibold border border-green-600 text-center rounded h-8 w-28 pt-0.5">Add Member</a>
