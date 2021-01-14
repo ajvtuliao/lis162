6bee3fb1f28faf3a7b3db1f39921c00cb7f25147
Alexandra Justine <59907792+ajvtuliao@users.noreply.github.com>
2021-01-14 17:09:29 +0800

    updated changes

--- resources/views/components/createform.blade.php
+++ resources/views/components/createform.blade.php
@@ -8,11 +8,11 @@
         </div>
         <form action="/create" method="post" class="flex flex-col space-y-2">
             @csrf
-            <label for="name">Name</label>
-            <input type="text" name="name" class="border-b" required>
-            <label for="date">Date of Membership</label>
+            <label for="name" class="text-gray-900 font-semibold">Name</label>
+            <input type="text" name="name" placeholder="First Name Last Name" class="border-b" required>
+            <label for="date" class="text-gray-900 font-semibold">Date of Membership</label>
             <input type="date" name="date" class="border-b" required>
-            <label for="status">Membership Status</label>
+            <label for="status" class="text-gray-900 font-semibold">Membership Status</label>
             <select name="status">
                 <option value="Active">Active</option>
                 <option value="Inactive">Inactive</option>
@@ -22,9 +22,9 @@
             </select>
     </div>
 
-    <label for="current">Current Projects</label>
+    <label for="current" class="text-gray-900 font-semibold">Current Projects</label>
     <input type="hidden" name="current" value="">
-    <textarea name="current" cols="30" rows="5" class="border border-black px-2 py-2"></textarea>
+    <textarea name="current" cols="30" rows="5" class="border border-gray-400 px-2 py-2" placeholder="Project 1, Project 2..."></textarea>
     <div class="flex flex-row justify-end">
         <button type="submit" class="border-indigo-800 bg-indigo-900 text-blue-50 font-semibold w-28 h-8 rounded-sm">Submit</button>
     </div>
--- resources/views/components/viewform.blade.php
+++ resources/views/components/viewform.blade.php
@@ -1,19 +1,19 @@
 <div class="flex flex-col space-y-4">
-    <h1 class="text-center text-2xl font-bold">{{ $member->name }}</h1>
+    <h1 class="text-center text-2xl font-bold text-gray-800">{{ $member->name }}</h1>
     <div class="flex flex-row space-x-4">
         <div class="flex flex-col items-center">
             <img src="{{ asset('img/user.svg') }}" alt="">
         </div>
         <form class="flex flex-col space-y-2">
             @csrf
-            <label for="date">Date of Membership</label>
-            <input type="text" name="date" class="border-b" required value="{{ $member->date }}" disabled>
-            <label for="status">Membership Status</label>
-            <input type="text" name="status" class="border-b" value="{{ $member->membership }}" disabled>
+            <label for="date" class="text-gray-900 font-semibold">Date of Membership</label>
+            <input type="text" name="date" class="border-b px-2 py-1" required value="{{ $member->date }}" disabled>
+            <label for="status" class="text-gray-900 font-semibold">Membership Status</label>
+            <input type="text" name="status" class="border-b px-2 py-1" value="{{ $member->membership }}" disabled>
         </form>
     </div>
 
-    <label for="current">Current Projects</label>
-    <textarea name="current" cols="30" rows="5" class="border border-black" disabled style="resize: none;">{{ $member->current }}</textarea>
+    <label for="current" class="text-gray-900 font-semibold">Current Projects</label>
+    <textarea name="current" cols="30" rows="5" class="px-2 py-1" disabled style="resize: none;">{{ $member->current }}</textarea>
     </form>
 </div>
